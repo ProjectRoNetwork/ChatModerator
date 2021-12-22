488825d87be19deb1d0d8e1e938d8dbad9265f7d
Ulric <57965846+daulric@users.noreply.github.com>
2021-12-22 18:09:22 -0400

    Just updated the Chat Moderator in the Roblox Lua Framework

--- chatmod.lua
+++ chatmod.lua
@@ -1,1 +1,21 @@
+--//Chat Logger\\--
+local url = "" -- Discord Webhook URL
 
+local http = game:GetService('HttpService')
+
+game.Players.PlayerAdded:connect(function(plr)
+
+	plr.Chatted:connect(function(msg)
+
+		local data = {
+			['username'] = plr.Name,
+			['content'] = msg
+		}
+
+		local newdata = http:JSONEncode(data)
+
+		http:PostAsync(url,newdata)
+
+	end)
+
+end)
